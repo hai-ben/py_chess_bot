12699105d6fe8a0e0e180a344f7c10df82c7f293
hai-ben <132700658+hai-ben@users.noreply.github.com>
2024-01-21 16:01:50 -0800

    Split up unit tests into more discrete modules

--- tests/main_engine/test_instruction_set_generation.py
+++ tests/main_engine/test_instruction_set_generation.py
@@ -1,183 +1,7 @@
-"""Unit Tests for the MainEngine class"""
-from collections import deque
+"""Tests that the instruction sets for each piece are generated correctly"""
 import pytest
-from tests.prototyping.pytest_resources import BASE_STATE_ASCII, START_STATE_ASCII
-from src.main_engine import MainEngine
 from src.resources.data_translators import SQUARE_IDX, B_KING_IDX, W_KING_IDX, CASTLE_IDX, EP_IDX,\
-    TURN_IDX, SQUARE_STATES, CASTLE_STATES, EP_FILE , PLAYER_TURN
-
-
-STARTING_LIST_STATE =\
-    [10, 8, 9, 11, 12, 9, 8, 10] + [7] * 8\
-    + [0] * 8 + [0] * 8 + [0] * 8 + [0] * 8\
-    + [1] * 8 + [4, 2, 3, 5, 6, 3, 2, 4]\
-    + [4] + [60] + [0b1111] + [-1] + [True]
-SHORT_INSTRUCTION = (3, STARTING_LIST_STATE[3], 9, STARTING_LIST_STATE[9])
-SINGLE_INSTRUCTION = (3, STARTING_LIST_STATE[3], 9, STARTING_LIST_STATE[9],
-                      0b1111, 0b1010, -1, 5)
-DOUBLE_INSTRUCTION = (3, STARTING_LIST_STATE[3], 9, STARTING_LIST_STATE[9],
-                       0b1111, 0b0000, -1, 2,
-                       15, STARTING_LIST_STATE[15], 20, STARTING_LIST_STATE[20])
-
-
-@pytest.fixture(name="engine")
-def fixture_blank_engine() -> MainEngine:
-    """Returns a MainEngine class with defaults"""
-    return MainEngine()
-
-
-@pytest.fixture(name="empty_board")
-def fixture_empty_board() -> MainEngine:
-    """Returns a board with no pieces and both king_idx set to a8"""
-    return MainEngine([0] * 66 + [0b1111] + [-1] + [True])
-
-
-def build_board(changed_tiles: list[tuple]) -> MainEngine:
-    """Updates the indicies of a empty MainEngine's state given the changed_tiles"""
-    board = MainEngine([0] * 66 + [0b1111] + [-1] + [True])
-    for idx, val in changed_tiles:
-        if not isinstance(idx, int):
-            idx = SQUARE_IDX[idx]
-        if not isinstance(val, (int, bool)):
-            val = SQUARE_STATES.get(
-                val, EP_FILE.get(val, PLAYER_TURN.get(val, SQUARE_IDX.get(val, None))))
-        board.state[idx] = val
-    return board
-
-
-def test_data_structs_init(engine: MainEngine):
-    """Tests to make sure the data structures match documentation"""
-    assert len(engine.state) == 69
-    assert isinstance(engine.game_graph, dict)
-    assert isinstance(engine.state_stack, deque)
-    assert isinstance(engine.hash_stack, deque)
-
-
-def test_default_start_state(engine: MainEngine):
-    """Makes sure the default starting state is a game of chess"""
-    assert engine.state == STARTING_LIST_STATE
-
-
-def test_init_with_data():
-    """Makes sure one can initiate a custom state"""
-    in_state = list([0] * 68 + [True])
-    engine = MainEngine(in_state)
-    assert engine.state == in_state
-
-
-def test_iter():
-    """Makes sure the iteration goes over nothing but the squares on the board"""
-    in_state = list([2] * 68 + [True])
-    engine = MainEngine(in_state)
-    idx = 0
-    for idx, x in enumerate(engine):
-        assert x == 2
-    assert idx == 63
-
-
-def test_str(engine: MainEngine):
-    """Makes sure the printout for the game_state is correct"""
-    print(engine)
-    assert str(engine) == START_STATE_ASCII
-    engine.state = [0] * 68 + [True]
-    assert str(engine) == BASE_STATE_ASCII
-
-
-def test_hash_basic(engine: MainEngine):
-    """Checks the hash of two fresh instances are the same"""
-    engine2 = MainEngine()
-    assert hash(engine) == hash(engine2)
-
-
-def test_hash_different_init(engine: MainEngine):
-    """Checks the hash of two identical custom instances are the same
-    and different from the starting hash"""
-    new_state = [0] * 68 + [True]
-    engine2 = MainEngine(new_state)
-    engine3 = MainEngine(new_state)
-    assert hash(engine2) != hash(engine)
-    assert hash(engine3) != hash(engine)
-    assert hash(engine2) == hash(engine3)
-
-
-def test_execture_short_instruction(engine: MainEngine):
-    """Tests that the engine propertly executes a short instruction set"""
-    start_hash = hash(engine)
-    engine.execute_instructions(SHORT_INSTRUCTION)
-    assert engine.state_stack[-1] == SHORT_INSTRUCTION  # Correctly stored the instruction set
-    assert engine.state[SHORT_INSTRUCTION[0]] == 0  # Correctly moved piece away
-    assert engine.state[SHORT_INSTRUCTION[2]] == SHORT_INSTRUCTION[1]  # Set new piece location
-    assert engine.state[66] == STARTING_LIST_STATE[66]  # Castling Rights
-    assert engine.state[67] == STARTING_LIST_STATE[67]  # En Passant File
-    assert engine.state[68] is False   # Player turn
-    assert start_hash != hash(engine)  # The hash has udpated
-    assert engine.game_graph[start_hash] == (SHORT_INSTRUCTION, hash(engine))
-
-
-def test_execute_instructions(engine: MainEngine):
-    """Tests that the engine properly executes long instruction set"""
-    start_hash = hash(engine)
-    engine.execute_instructions(SINGLE_INSTRUCTION)
-    assert engine.state_stack[-1] == SINGLE_INSTRUCTION  # Correctly stored the instruction set
-    assert engine.state[3] == 0  # Correctly moved piece away
-    assert engine.state[9] == SINGLE_INSTRUCTION[1]  # Correctly set new piece location
-    assert engine.state[66] == 0b1010  # Castling Rights
-    assert engine.state[67] == 5       # En Passant File
-    assert engine.state[68] is False   # Player turn
-    assert start_hash != hash(engine)  # The hash has udpated
-    assert engine.game_graph[start_hash] == (SINGLE_INSTRUCTION, hash(engine))
-
-
-def test_execute_double_instructions(engine: MainEngine):
-    """Tests that the engine properly executes a double (castling) instruction set"""
-    start_hash = hash(engine)
-    engine.execute_instructions(DOUBLE_INSTRUCTION)
-    assert engine.state_stack[-1] == DOUBLE_INSTRUCTION  # Correctly stored the instruction set
-    assert engine.state[3] == 0  # Correctly moved piece away
-    assert engine.state[9] == DOUBLE_INSTRUCTION[1]  # Correctly set new piece location
-    assert engine.state[15] == 0  # Correctly moved piece away
-    assert engine.state[20] == DOUBLE_INSTRUCTION[9]  # Correctly set new piece location
-    assert engine.state[66] == 0b0000  # Castling Rights
-    assert engine.state[67] == 2       # En Passant File
-    assert engine.state[68] is False   # Player turn
-    assert start_hash != hash(engine)  # The hash has udpated
-    assert engine.game_graph[start_hash] == (DOUBLE_INSTRUCTION, hash(engine))
-
-
-def test_reverse_last_instruction(engine: MainEngine):
-    """Ensures that a simple instruction is correctly undone"""
-    start_hash = hash(engine)
-    engine.execute_instructions(SINGLE_INSTRUCTION)
-    engine.reverse_last_instruction()
-    assert engine.state == STARTING_LIST_STATE
-    assert hash(engine) == start_hash
-    assert len(engine.state_stack) == 0
-
-
-def test_reverse_last_double_instruction(engine: MainEngine):
-    """Ensures that a double instruction is correctly undone"""
-    start_hash = hash(engine)
-    engine.execute_instructions(DOUBLE_INSTRUCTION)
-    engine.reverse_last_instruction()
-    assert engine.state == STARTING_LIST_STATE
-    assert hash(engine) == start_hash
-    assert len(engine.state_stack) == 0
-
-
-def test_king_idx_updated_with_instructions(engine: MainEngine):
-    """Ensures that the king indicies are updated when king move instructions are executed"""
-    # White king movement
-    instruction_set_1 = (60, engine.state[60], 35, engine.state[35])
-    engine.execute_instructions(instruction_set_1)
-    assert engine.state[65] == 35  # White King
-    assert engine.state[64] == 4   # Black King
-
-    # Black king movement
-    instruction_set_2 = (4, engine.state[4], 20, engine.state[20])
-    engine.execute_instructions(instruction_set_2)
-    assert engine.state[64] == 20  # Black King
-    assert engine.state[65] == 35  # White King
-
+    TURN_IDX, SQUARE_STATES, CASTLE_STATES, EP_FILE
 
 MOVE_TEST_DICT = {
     "WHITE_KING_EMPTY": (
@@ -865,16 +689,21 @@ MOVE_TEST_DICT = {
 }
 
 
+def test_castle_threatened():
+    # TODO:
+    pass
+
+
 # Using the move test dict keys as test parameters for easier debugging
 @pytest.mark.parametrize("test_key", MOVE_TEST_DICT.keys())
-def test_get_moves(test_key: str):
+def test_get_moves(board_state_generator, test_key: str):
     """After making modifications to empty board, this calls func_name of empty board with args
     and asserts the set of that return value is the same as the set of the expected_instructions"""
     # Unpack the test information
     mods, instruction_gen_args, (func, args) = MOVE_TEST_DICT[test_key]
 
     # Unpack the modifications to the state and apply them
-    board = build_board(mods)
+    board = board_state_generator(mods)
 
     # Generate the instruction set
     if len(instruction_gen_args) == 4:
@@ -921,118 +750,3 @@ def test_get_moves(test_key: str):
     for move in expected_instructions:
         assert move in actual_instructions
     assert actual_instructions == expected_instructions
-
-
-STATE_RETURN_DICT = {
-    "WHITE_CHECKED_BY_B_ROOK_H": (
-        [("d2", "w_king"), (W_KING_IDX, "d2"), ("f2", "b_rook")],
-        ("in_check", [True]),
-        True
-    ),
-    "WHITE_NOT_CHECKED_BY_W_ROOK_H": (
-        [("d2", "w_king"), (W_KING_IDX, "d2"), ("f2", "w_rook")],
-        ("in_check", [True]),
-        False
-    ),
-}
-
-
-# Using the board_state dict keys as test parameters for easier debugging
-@pytest.mark.parametrize("test_key", STATE_RETURN_DICT.keys())
-def test_state_return(test_key: str):
-    """Modifies the state of an empty board to according to mods and then
-    calls the given function with the arguments and checks it against the
-    specified expected state"""
-    mods, (func, args), expected_state = STATE_RETURN_DICT[test_key]
-    board = build_board(mods)
-    print(board)
-    assert getattr(board, func)(*args) == expected_state
-
-
-def test_in_check_rook():
-    # TODO:
-    pass
-
-
-def test_in_check_bishop():
-    # TODO:
-    pass
-
-
-def test_in_check_knight():
-    # TODO:
-    pass
-
-
-def test_in_check_king():
-    # TODO:
-    pass
-
-
-def test_in_check_queen():
-    # TODO:
-    pass
-
-
-def test_in_check_pawn():
-    # TODO:
-    pass
-
-
-def test_castle_threatened():
-    # TODO:
-    pass
-
-
-def test_pin():
-    # TODO:
-    pass
-
-
-def test_hash_moves():
-    # TODO:
-    pass
-
-
-def test_hash_take():
-    # TODO:
-    pass
-
-
-def test_hash_circular():
-    # TODO:
-    pass
-
-
-def test_hash_castling():
-    # TODO:
-    pass
-
-
-def test_hash_enpassant():
-    # TODO:
-    pass
-
-
-def test_sufficient_material():
-    # TODO:
-    pass
-
-
-def test_get_game_notation():
-    # TODO:
-    pass
-
-
-def test_play_game_from_notation():
-    # TODO
-    pass
-
-
-def test_hash_pawn_promote():
-    pass
-
-
-def test_hash_en_passant_take():
-    # TODO
-    pass
---
+++ tests/main_engine/test_threat_detection.py
@@ -0,0 +1,84 @@
+import pytest
+from src.resources.data_translators import SQUARE_IDX, B_KING_IDX, W_KING_IDX, CASTLE_IDX, EP_IDX,\
+    TURN_IDX, SQUARE_STATES, CASTLE_STATES, EP_FILE
+
+
+STATE_RETURN_DICT = {
+    "WHITE_CHECKED_BY_B_ROOK_H": (
+        [("d2", "w_king"), (W_KING_IDX, "d2"), ("f2", "b_rook")],
+        ("in_check", [True]),
+        True
+    ),
+    "WHITE_NOT_CHECKED_BY_W_ROOK_H": (
+        [("d2", "w_king"), (W_KING_IDX, "d2"), ("f2", "w_rook")],
+        ("in_check", [True]),
+        False
+    ),
+    "BLACK_CHECKED_BY_W_ROOK_H": (
+        [("e3", "w_king"), (B_KING_IDX, "e3"), ("a3", "w_rook")],
+        ("in_check", [False]),
+        True
+    ),
+    "BLACK_NOT_CHECKED_BY_B_ROOK_H": (
+        [("e3", "w_king"), (B_KING_IDX, "e3"), ("a3", "b_rook")],
+        ("in_check", [False]),
+        False
+    ),
+    "WHITE_CHECKED_BY_B_ROOK_V": (
+        [("a2", "w_king"), (W_KING_IDX, "a2"), ("a8", "b_rook")],
+        ("in_check", [True]),
+        True
+    ),
+    "WHITE_NOT_CHECKED_BY_W_ROOK_V": (
+        [("a2", "w_king"), (W_KING_IDX, "a2"), ("a8", "w_rook")],
+        ("in_check", [True]),
+        False
+    ),
+    "BLACK_CHECKED_BY_W_ROOK_V": (
+        [("h8", "w_king"), (B_KING_IDX, "h8"), ("h1", "w_rook")],
+        ("in_check", [False]),
+        True
+    ),
+    "BLACK_NOT_CHECKED_BY_B_ROOK_V": (
+        [("h8", "w_king"), (B_KING_IDX, "h8"), ("h1", "b_rook")],
+        ("in_check", [False]),
+        False
+    ),
+}
+
+
+def test_in_check_bishop():
+    # TODO:
+    pass
+
+
+def test_in_check_knight():
+    # TODO:
+    pass
+
+
+def test_in_check_king():
+    # TODO:
+    pass
+
+
+def test_in_check_queen():
+    # TODO:
+    pass
+
+
+def test_in_check_pawn():
+    # TODO:
+    pass
+
+
+# Using the board_state dict keys as test parameters for easier debugging
+@pytest.mark.parametrize("test_key", STATE_RETURN_DICT.keys())
+def test_state_return(board_state_generator, test_key: str):
+    """Modifies the state of an empty board to according to mods and then
+    calls the given function with the arguments and checks it against the
+    specified expected state"""
+    mods, (func, args), expected_state = STATE_RETURN_DICT[test_key]
+    board = board_state_generator(mods)
+    print(board)
+    assert getattr(board, func)(*args) == expected_state
